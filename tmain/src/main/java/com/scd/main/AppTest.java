4676da0b6f0fc51f13a96efc5dc4ed9410ebc076
chengdu <3281328128@qq.com>
2019-07-20 23:15:39 +0800

    init project

---
+++ tmain/src/main/java/com/scd/main/AppTest.java
@@ -0,0 +1,12 @@
+package com.scd.main;
+
+/**
+ * @author chengdu
+ * @date 2019/7/20.
+ */
+public class AppTest {
+
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
